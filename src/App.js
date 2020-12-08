eb9f8c2de67ad63782e90c12c9b8aafb488beb70
trombox <nicotornougues@gmail.com>
2020-12-08 14:58:09 -0300

    se agrega slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./componentes/NavBar";
-
+import Slider from "./componentes/Slider";
 function App() {
   return (
     <div>
    <Navbar/>
+   <Slider/>
       </div>
     
   );
