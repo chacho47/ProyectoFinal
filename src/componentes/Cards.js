cd426381ea6da8d32663952b29d29e54fd0243aa
trombox <nicotornougues@gmail.com>
2020-12-09 19:52:35 -0300

    se da formato a las cards

--- src/componentes/Cards.js
+++ src/componentes/Cards.js
@@ -1,50 +1,52 @@
 import React from "react";
 import CardDeck from "react-bootstrap/CardDeck";
 import Card from "react-bootstrap/Card";
+import "../estilos/cards.css";
+import mano from "../img/CardDeck/mano.jpg";
+import traumatologia from "../img/CardDeck/traumatologia.jpg";
+import valle from "../img/CardDeck/valle.jpg";
 const Cards = () => {
   return (
-    <CardDeck>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{" "}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardDeck>
+    <div className="container text-center mb-5">
+      <CardDeck>
+        <Card>
+          <Card.Img variant="top" src={mano} />
+          <Card.Body>
+            <Card.Title>Cirugía de Mano</Card.Title>
+            <Card.Text>Reconstructiva y de miembro superior</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={traumatologia} />
+          <Card.Body>
+            <Card.Title>Muestras</Card.Title>
+            <Card.Text>
+              Incorporamos novedosa técnica de extracción de muestras al vacío
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={valle} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
+    </div>
   );
 };
 
--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,83 +1,84 @@
-import React from 'react';
-import logoclinica from '../img/logoclinica.png';
-import uno from '../img/uno.png';
-import amanus from '../img/amanus.png';
-import lafarmacia from '../img/lafarmacia.png';
-import vallesalud from '../img/vallesalud.png';
-import emec from '../img/emec.png';
-import clinicasarmiento from '../img/clinicasarmiento.png';
-import '../estilos/footer.css';
+import React from "react";
+import logoclinica from "../img/logoclinica.png";
+import uno from "../img/uno.png";
+import amanus from "../img/amanus.png";
+import lafarmacia from "../img/lafarmacia.png";
+import vallesalud from "../img/vallesalud.png";
+import emec from "../img/emec.png";
+import clinicasarmiento from "../img/clinicasarmiento.png";
+import "../estilos/footer.css";
 
 const Footer = () => {
-    return (
-        <footer className='justify-content-center'>
-            <section className='bg-dark py-3 text-center prefooter'>
-                <div className='row'>
-                    <div className='col-md-4 col-sm-6 mb-3'>
-                        <img className='w-25' src={amanus} alt=""/>
-                    </div>
-                    <div className='col-md-4 col-sm-6 mb-3'>
-                        <img className='w-25' src={uno} alt=""/>
-                    </div>
-                    <div className='col-md-4 col-sm-6 mb-3'>
-                        <img className='w-25' src={lafarmacia} alt=""/>
-                    </div>
-                    <div className='col-md-4 col-sm-6'>
-                        <img className='w-25' src={vallesalud} alt=""/>
-                    </div>
-                    <div className='col-md-4 col-sm-6'>
-                        <img className='w-25' src={emec} alt=""/>
-                    </div>
-                    <div className='col-md-4 col-sm-6'>
-                        <img className='w-25' src={clinicasarmiento} alt=""/>
-                    </div>
-                </div>
-            </section>
-            <section className='bg-light text-dark'>
-                <div className='container'>
-                <div className='row'>
-                <div className='col-lg-3 col-md-6 col-sm-12'>
-                            <h3 className='mb-3'>Nosotros</h3>
-                            <img
-                             className='w-50'
-                             src={logoclinica} 
-                             alt="logo de la clinica"/>
-                    </div>
-                    <div className='col-lg-3 col-md-6 col-sm-12'>
-                        <h3 className='mb-3'>Clinica Favaloro</h3>
-                        <ul className='list-unstyled'>
-                            <li>Inicio</li>
-                            <li>Nuestra empresa</li>
-                            <li>Novedades</li>
-                            <li>Contactos</li>
-                        </ul>
-                    </div>
-                    <div className='col-lg-3 col-md-6 col-sm-12'>
-                        <h3 className='mb-3'>Servicios</h3>
-                        <ul className='list-unstyled'>
-                            <li>Pacientes</li>
-                            <li>Nuestros profesionales</li>
-                            <li>Residencias Medicas</li>
-                        </ul>
-                    </div>
-                    <div className='col-lg-3 col-md-6 col-sm-12'>
-                        <h3 className='mb-3'>Informacion Util</h3>
-                        <ul className='list-unstyled'>
-                            <li>Turnos</li>
-                            <li>Preguntas frecuentes</li>
-                            <li>Horarios de atencion</li>
-                            <li>Trabaja con nosotros</li>
-                        </ul>
-                    </div>
-                </div>
-                </div>
-                <hr/>
-                <div className='Container justify-content text-center asd'>
-                    <p>Todos los derechos reservados - Comision 2i</p>
-                </div>
-            </section>
-        </footer>
-    );
+  return (
+    <footer className="justify-content-center">
+      <section className="py-3 text-center prefooter">
+        <div className="row">
+          <div className="col-md-4 col-sm-6 mb-3">
+            <img className="w-25" src={amanus} alt="" />
+          </div>
+          <div className="col-md-4 col-sm-6 mb-3">
+            <img className="w-25" src={uno} alt="" />
+          </div>
+          <div className="col-md-4 col-sm-6 mb-3">
+            <img className="w-25" src={lafarmacia} alt="" />
+          </div>
+          <div className="col-md-4 col-sm-6">
+            <img className="w-25" src={vallesalud} alt="" />
+          </div>
+          <div className="col-md-4 col-sm-6">
+            <img className="w-25" src={emec} alt="" />
+          </div>
+          <div className="col-md-4 col-sm-6">
+            <img className="w-25" src={clinicasarmiento} alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="bg-light text-dark">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-md-6 col-sm-12">
+              <h3 className="mb-3">Nosotros</h3>
+              <img
+                className="w-50"
+                src={logoclinica}
+                alt="logo de la clinica"
+              />
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+              <h3 className="mb-3">Clinica Favaloro</h3>
+              <ul className="list-unstyled">
+                <li>Inicio</li>
+                <li>Nuestra empresa</li>
+                <li>Novedades</li>
+                <li>Contactos</li>
+              </ul>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+              <h3 className="mb-3">Servicios</h3>
+              <ul className="list-unstyled">
+                <li>Pacientes</li>
+                <li>Nuestros profesionales</li>
+                <li>Residencias Medicas</li>
+              </ul>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+              <h3 className="mb-3">Informacion Util</h3>
+              <ul className="list-unstyled">
+                <li>Turnos</li>
+                <li>Preguntas frecuentes</li>
+                <li>Horarios de atencion</li>
+                <li>Trabaja con nosotros</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="Container justify-content text-center asd">
+          <p>Todos los derechos reservados - Comision 2i</p>
+        </div>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
