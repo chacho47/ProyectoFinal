26b9e57eca525cdc27161cced33e307062f1ca7f
Emilse Arias <nachsalas93@gmail.com>
2020-12-09 18:34:30 -0300

    se agregaron iconos al componente Presentacion y se instalo fontawesome y react-icons

--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { FaHospitalAlt } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { FaHeartbeat } from "react-icons/fa";
+import { FaBookReader } from "react-icons/fa";
 
 const Presentacion = () => {
   return (
@@ -6,8 +10,15 @@ const Presentacion = () => {
       <div className="container">
         <div className="row text-center shadow p-3 mb-5 bg-white rounded">
           <div className="col-sm-12 col-md-4">
+            <IconContext.Provider
+              value={{ style: { fontSize: "40px", color: "rgb(0, 123, 255)" } }}
+            >
+              <div>
+                <FaHospitalAlt />
+              </div>
+            </IconContext.Provider>
             <p className="lead">Infraestructura</p>
-            <hr/>
+            <hr />
             <p>
               3 torres de atención a pacientes, 7 pisos de internación, 3 sedes
               de consulta médica. Seguimos creciendo al ritmo de la región, para
@@ -15,8 +26,15 @@ const Presentacion = () => {
             </p>
           </div>
           <div className="col-sm-12 col-md-4">
+          <IconContext.Provider
+              value={{ style: { fontSize: "40px", color: "rgb(0, 123, 255)" } }}
+            >
+              <div>
+                <FaHeartbeat />
+              </div>
+            </IconContext.Provider>
             <p className="lead">Innovacion</p>
-            <hr/>
+            <hr />
             <p>
               Incorporamos permanentemente tecnología de punta para garantizar
               prestaciones de la más alta calidad, a la altura de los mejores
@@ -24,8 +42,15 @@ const Presentacion = () => {
             </p>
           </div>
           <div className="col-sm-12 col-md-4">
+          <IconContext.Provider
+              value={{ style: { fontSize: "40px", color: "rgb(0, 123, 255)" } }}
+            >
+              <div>
+                <FaBookReader />
+              </div>
+            </IconContext.Provider>
             <p className="lead">Capacitación</p>
-            <hr/>
+            <hr />
             <p>
               Nuestro recurso humano recibe capacitación permanente para brindar
               un servicio diferencial, en todos los estadios de atención.
