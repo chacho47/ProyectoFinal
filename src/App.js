814885d9e74cc812073c0e6d4d8a04f07e18e66d
trombox <nicotornougues@gmail.com>
2020-12-14 12:19:48 -0300

    mergin trombox con dev

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Link, NavLink } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./componentes/Footer";
