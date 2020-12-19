2479805e3d24da4380993a24c7c7ee4c5af9b947
trombox <nicotornougues@gmail.com>
2020-12-19 18:44:45 -0300

    se corrige covid con .find

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import FormPaciente from "./componentes/FormPaciente";
 import Home from "./componentes/Home";
+import Footer from "./componentes/Footer";
+import Header from "./componentes/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/registrarme" component={FormPaciente} />
       </Switch>
+      <Footer />
     </BrowserRouter>
-    
   );
 }
 
