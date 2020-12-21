69f254442b0b5e69ca89dc7b1af840803cbcf5c5
trombox <nicotornougues@gmail.com>
2020-12-21 19:34:20 -0300

    se crea portal medico y paciente

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from "./componentes/Home";
 import Footer from "./componentes/Footer";
 import Header from "./componentes/Header";
 import AdminUsuario from "./componentes/AdminUsuario";
+import PacienteTurnos from "./componentes/PacienteTurnos";
+import MedicoTurnos from "./componentes/MedicoTurnos";
+import AltaMedico from "./componentes/AltaMedico";
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/registrarme" component={FormPaciente} />
-        <Route path="/lista-usuarios" component={AdminUsuario}/>
+        <Route path="/lista-usuarios" component={AdminUsuario} />
+        <Route path="/alta-medico" component={AltaMedico} />
+        <Route path="/paciente-turnos" component={PacienteTurnos} />
+        <Route path="/medico-turnos" component={MedicoTurnos} />
       </Switch>
       <Footer />
     </BrowserRouter>
---
+++ src/componentes/PacienteTurnos/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PacienteTurnos = () => {
+  return (
+    <div>
+      <h1>desde paciente turnos</h1>
+    </div>
+  );
+};
+
+export default PacienteTurnos;
